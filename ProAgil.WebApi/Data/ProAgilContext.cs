13f57d160d3c6ffe5caa1e2b075cca73c57fd38d
Humberto Freitas <humberto.freitas@antt.gov.br>
2020-06-29 12:42:56 -0300

    Adicionando o ProAgilContext e a injenção de dependencia.

---
+++ ProAgil.WebApi/Data/ProAgilContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProAgil.WebApi.Model;
+
+namespace ProAgil.WebApi.Data
+{
+    public class ProAgilContext: DbContext
+    {
+        public ProAgilContext(DbContextOptions<ProAgilContext> options) : base(options) { }
+
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
