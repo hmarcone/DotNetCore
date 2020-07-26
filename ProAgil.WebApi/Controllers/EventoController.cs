695ecd7f95c843daf0211f6991109157c869ee6d
Humberto Marcone <hmarcone@gmail.com>
2020-07-26 20:32:58 -0300

    Ajustes metódo de upload

--- ProAgil.WebApi/Controllers/EventoController.cs
+++ ProAgil.WebApi/Controllers/EventoController.cs
@@ -5,7 +5,6 @@ using ProAgil.Domain.Entities;
 using ProAgil.Infrastructure.DbModels;
 using ProAgil.Repository;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -43,35 +42,31 @@ namespace ProAgil.WebApi.Controllers
 		}
 
 		[HttpPost("upload")]
-        public async Task<IActionResult> Upload()
-        {
-            try
-            {
-                var file = Request.Form.Files[0];
-                var folderName = Path.Combine("Resources", "Images");
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+		public async Task<IActionResult> Upload()
+		{
+			try
+			{
+				var file = Request.Form.Files[0];
+				var folderName = Path.Combine("Resources", "Images");
+				var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
 
-                if (file.Length > 0)
-                {
-                    var filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName;
-                    var fullPath = Path.Combine(pathToSave, filename.Replace("\"", " ").Trim());
+				if (file.Length > 0)
+				{
+					var filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName;
+					var fullPath = Path.Combine(pathToSave, filename.Replace("\"", " ").Trim());
 
-                    using (var stream = new FileStream(fullPath, FileMode.Create))
-                    {
-                        file.CopyTo(stream);
-                    }
-                }
+					using var stream = new FileStream(fullPath, FileMode.Create);
+					file.CopyTo(stream);
+				}
 
-                return Ok();
-            }
-            catch (System.Exception ex)
-            {
-                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Banco Dados Falhou {ex.Message}");
-            }
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, $"Falha ao realiza o upload da imagem. Erro: {ex.Message}");
+			}
+		}
 
-            return BadRequest("Erro ao tentar realizar upload");
-        }
-		
 		[HttpGet("{EventoId}")]
 		public async Task<IActionResult> Get(int EventoId)
 		{
