d8c7596b37e0614de92371474cb10841c75185b1
Humberto Freitas <humberto.freitas@antt.gov.br>
2020-08-07 18:35:02 -0300

    Adicionando os eventos e redes sociais

--- ProAgil-App/src/app/eventos/eventoEdit/eventoEdit.component.ts
+++ ProAgil-App/src/app/eventos/eventoEdit/eventoEdit.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { EventoService } from 'src/app/_services/evento.service';
 import { BsModalService } from 'ngx-bootstrap/modal';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 import { ToastrService } from 'ngx-toastr';
 import { Evento } from 'src/app/_models/Evento';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-evento-edit',
@@ -22,23 +23,60 @@ export class EventoEditComponent implements OnInit {
   fileNameToUpdate: string;
 
   dataAtual = '';
+
+  get lotes(): FormArray {
+    return <FormArray>this.registerForm.get('lotes');
+  }
+
+  get redesSociais(): FormArray {
+    return <FormArray>this.registerForm.get('redesSociais');
+  }
+
   constructor(
     private eventoService: EventoService
   , private modalService: BsModalService
   , private fb: FormBuilder
   , private localeservice: BsLocaleService
   , private toastr: ToastrService
+  , private router: ActivatedRoute
   )
   {
     this.localeservice.use('pt-br');
   }
 
+  // tslint:disable-next-line:typedef
   ngOnInit() {
     this.validation();
+    this.carregarEvento();
+  }
+
+  // tslint:disable-next-line:typedef
+  carregarEvento() {
+    const idEvento = +this.router.snapshot.paramMap.get('id');
+    this.eventoService.getEventoById(idEvento)
+      .subscribe(
+        (evento: Evento) => {
+          this.evento = Object.assign({}, evento);
+          this.fileNameToUpdate = evento.imagemURL.toString();
+
+          this.imagemURL = `http://localhost:5000/resources/images/${this.evento.imagemURL}?_ts=${this.dataAtual}`;
+
+          this.evento.imagemURL = '';
+          this.registerForm.patchValue(this.evento);
+
+          this.evento.lotes.forEach(lote => {
+            this.lotes.push(this.criaLote(lote));
+          });
+          this.evento.redesSociais.forEach(redeSocial => {
+            this.redesSociais.push(this.criaRedeSocial(redeSocial));
+          });
+        }
+      );
   }
 
   validation() {
     this.registerForm = this.fb.group({
+      id: [],
       nome: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
       local: ['', Validators.required],
       dataEvento: ['', Validators.required],
@@ -46,18 +84,44 @@ export class EventoEditComponent implements OnInit {
       qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
       telefone: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      lotes: this.fb.group({
-        nome: ['', Validators.required],
-        quantidade: ['', Validators.required],
-        preco: ['', Validators.required],
-        dataInicio: [''],
-        dataFim: ['']      
-      }),
-      redesSociais: this.fb.group({
-        nome: ['', Validators.required],
-        url: ['', Validators.required]
-      })
+      lotes: this.fb.array([]),
+      redesSociais: this.fb.array([])
     });
+  }
+
+  criaLote(lote: any): FormGroup {
+    return this.fb.group({
+      id: [lote.id],
+      nome: [lote.nome, Validators.required],
+      quantidade: [lote.quantidade, Validators.required],
+      preco: [lote.preco, Validators.required],
+      dataInicio: [lote.dataInicio],
+      dataFim: [lote.dataFim]
+    });
+  }
+
+  criaRedeSocial(redeSocial: any): FormGroup {
+    return this.fb.group({
+      id: [redeSocial.id],
+      nome: [redeSocial.nome, Validators.required],
+      url: [redeSocial.url, Validators.required]
+    });
+  }
+
+  adicionarLote() {
+    this.lotes.push(this.criaLote({ id: 0 }));
+  }
+
+  adicionarRedeSocial() {
+    this.redesSociais.push(this.criaRedeSocial({ id: 0 }));
+  }
+
+  removerLote(id: number) {
+    this.lotes.removeAt(id);
+  }
+
+  removerRedeSocial(id: number) {
+    this.redesSociais.removeAt(id);
   }
 
   onFileChange(evento: any, file: FileList) {
